b4608c72eb80d5d570d75fd1fcd7dce08efe7436
Sullivan677 <decarli.sulli@gmail.com>
2021-04-22 12:24:33 +0200

    Initial Commit

---
+++ TodoFirebase/TodoFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoFirebaseApp.swift
+//  TodoFirebase
+//
+//  Created by Sullivan De carli on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
